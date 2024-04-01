f815ccf94aa5819f7dc105165f02ee423021b51c
Kelwin Esechiel <90807426+kelwinxd@users.noreply.github.com>
2024-04-01 18:49:57 -0300

    up

---
+++ Easter3d/README.md
@@ -0,0 +1,25 @@
+# Easter Explanation Website
+
+This project is a website created to explain the significance of Easter. It utilizes HTML5, CSS, and JavaScript to create an interactive web experience that educates visitors about the meaning of Easter.
+
+![Easter Bunny](./assets/project.png "Project")
+
+## Project Overview
+
+Easter is a significant religious holiday celebrated by Christians worldwide. This website aims to provide an explanation of the true meaning of Easter, focusing on the death and resurrection of Jesus Christ.
+
+The website features the following components:
+
+- **HTML5**: The structure of the website is built using HTML5 to create semantic markup for various elements such as headers, paragraphs, and lists.
+
+- **CSS**: Cascading Style Sheets (CSS) are used to style the website, providing visual enhancements such as colors, fonts, and layout formatting.
+
+- **JavaScript**: JavaScript is used to add interactivity to the website, allowing for dynamic content updates and user interaction. For example, JavaScript may be used to implement animations, event handling, and form validation.
+
+## Usage
+
+To view the website locally, simply clone this repository and open the `index.html` file in a web browser.
+
+```bash
+git clone https://github.com/seu-usuario/seu-repositorio.git
+cd seu-repositorio
--- Easter3d/script.js
+++ Easter3d/script.js
@@ -1,11 +1,65 @@
+const title = document.querySelector('.title')
+const phrase = "Do you know the meaning of Easter?"
+const desc = document.querySelector('.desc-egito')
+const sec2 = document.querySelectorAll('.slide')
+const rock = document.querySelector('.model3')
+const desc2 = document.querySelector('.desc-res')
+const sec3 = document.querySelector('.section3')
+
+console.log(rock)
+
+function openRock(){
+    rock.play()
+    setTimeout(() => {
+        rock.pause()
+        desc2.classList.add('appear')
+        setTimeout(() => { desc2.textContent = "...He was not there. He has risen!"},100)
+       sec3.classList.add('wakeup')
+       window.scrollBy({
+        top: window.innerHeight + 20, // Mude o valor conforme desejado
+        left: 0,
+        behavior: 'smooth' 
+       })
+       
+       
+    },1800)
+
+    setTimeout(() => {
+        document.querySelector('.final-title').classList.add('opa')
+    },2000)
+
+    rock.removeEventListener('click',openRock)
+}
+
+console.log(window.innerHeight)
+rock.addEventListener('click', openRock)
+
+let index = 0
+
+function type(){
+    title.textContent = phrase.slice(0, index++)
+
+    if(index <= phrase.length){
+        setTimeout(type, 90)
+    }
+    
+    if (index == phrase.length){
+        title.classList.add('bar')
+    }
+}
 
 
 window.onload = function() {
     const pyramid = document.querySelector('.model2');
     pyramid.play();
+    
     setTimeout(() => {
         pyramid.pause();
-    }, 5000);
+    }, 4000);
+
+    type()
+    setTimeout(()=>{ desc.classList.add('ativo')},4100)
+   
 
     console.log(pyramid)
 };
@@ -20,13 +74,35 @@ function handleScroll() {
 
     if (scrollY > 100) {
         document.querySelector('.model').play();
+        
+      
+        
+        sec2.forEach((e) => {
+            e.classList.add('come')
+        })
+        
+            
+        
         setTimeout(() => {
             document.querySelector('.model').pause();
+         
+            
         }, 1600);
+
+     
+    
+
+
+       
+       
+        
 
         window.removeEventListener('scroll', handleScroll);
         window.removeEventListener('touchmove', handleScroll);
     }
+
+
+ 
 }
 
 window.addEventListener('scroll', handleScroll);
